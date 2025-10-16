bc7e0c7856c3046509c6f610f7a06475e095e827
pedrochoks <pedro.a.gomes12@aluno.senai.br>
2025-10-16 10:49:40 -0300

    Inicio do C#

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Introducao_C_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bem Vindo(a) ao sistema.");
+
+        //Entrada de dados
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nOla, {nome}! Voce tem {idade} anos.");
+
+        if (idade < 18)
+        {
+            Console.WriteLine("Voce ainda de e menor de idade");
+        }
+        else
+        {
+            Console.WriteLine("Voce ja e maior de idade");
+        }
+
+        // Menu
+        Console.WriteLine("\nEscolha uma opcao: ");
+        Console.WriteLine("1 - Ver a tabuada de um numero: ");
+        Console.WriteLine("2 - Contar ate um numero: ");
+        Console.WriteLine("3 - Sair");
+        Console.WriteLine("Digite a sua opcao:");
+        int opcao = int.Parse(Console.ReadLine());
+    }
+}
