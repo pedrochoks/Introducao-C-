3838095403e7f5b21d53709c8b44dc453f63fad2
pedrochoks <pedro.a.gomes12@aluno.senai.br>
2025-10-17 08:25:56 -0300

    Exercicios 17-10

--- Program.cs
+++ Program.cs
@@ -31,5 +31,43 @@ class Program
         Console.WriteLine("3 - Sair");
         Console.WriteLine("Digite a sua opcao:");
         int opcao = int.Parse(Console.ReadLine());
+        switch (opcao)
+        {
+            case 1:
+                Console.WriteLine("Digite o numero que deseja ver a tabuada: ");
+                int numero = int.Parse(Console.ReadLine());
+                Console.WriteLine("A tabuada deste numero é: ");
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine(i * numero);
+                }
+                break;
+
+            case 2:
+                Console.WriteLine("Digite ate o numero que deseja contar: ");
+                int numeroContar = int.Parse(Console.ReadLine());
+
+                for (int n = 0; n <= numeroContar; n++)
+                {
+                    Console.WriteLine(n);
+                }
+                break;
+
+            case 3:
+                Console.WriteLine("Voce saiu");
+                break;
+        }
+
+        //* Sequencia de fibonacci
+        int numeroAtual = 1;
+        int numeroAnterior = 0;
+
+        for (int i = 0; i < 11; i++)
+        {
+            int resultado = numeroAnterior + numeroAtual;
+            numeroAnterior = numeroAtual;
+            numeroAtual = resultado;
+            Console.WriteLine(resultado);
+        }
     }
 }
--- obj/Debug/net9.0/Introducao C#.AssemblyInfo.cs
+++ obj/Debug/net9.0/Introducao C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Introducao C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b139434a5a6fbcfffc3382a5fc903d2b4f3cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Introducao C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Introducao C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
